7a5fbc80bf2ac41fe7fbbdd6a63c8e7fb6534fc5
Mikhail8425 <mikhailshemet@gmail.com>
2024-02-06 15:05:39 -0500

    group by

--- test.sql
+++
@@ -1,9 +0,0 @@
--- SELECT students.name as student_name, email, cohorts.name as cohort_name
--- FROM students JOIN cohorts ON cohort_id = cohorts.id;
-
--- INNER JOIN
--- SELECT students.name as student_name, email, cohorts.name as cohort_name
--- FROM students INNER JOIN cohorts ON cohorts.id = cohort_id;
--- -- OUTER JOIN, must be LEFT, RIGHT, or FULL
-SELECT students.name as student_name, email, cohorts.name as cohort_name
-FROM students LEFT OUTER JOIN cohorts ON cohorts.id = cohort_id;
