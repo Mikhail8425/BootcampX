8fc853bb9235863697faa2aa9ade47ccfad44976
Mikhail8425 <mikhailshemet@gmail.com>
2024-02-08 16:41:37 -0500

    bootcamp_app

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,30 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "labber",
+  password: "labber",
+  host: "localhost",
+  database: "bootcamp",
+});
+
+const queryString = `
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM teachers
+  JOIN assistance_requests ON teacher_id = teachers.id
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name LIKE $1
+  ORDER BY teacher;
+`
+
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`];
+
+pool
+.query(queryString,values)
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+});
